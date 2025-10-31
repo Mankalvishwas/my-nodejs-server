049e08a0711128c77568a9adc0c52e16038e3638
mankalvishwas <mankalvishwas@gmail.com>
2025-10-31 18:14:23 +0530

    Add lostItem model for MongoDB

--- models/LostItem.js
+++ models/LostItem.js
@@ -1,18 +1,11 @@
-const mongoose=require('mongoose');
+// models/lostItem.js
+const mongoose = require('mongoose');
 
-
-const lostitemschema=new mongoose.Schema({
-    title:{type:String,required:true},
-    description:{type:String,required:true},
-    location:{type:String,required:true},
-    dateLost:{type:Date,required:true},
-    reporterName:{type:String,required:true},
-    contactInfo:{type:String,unique:true,lowercase:true,required:true},
-    isFound:{type:Boolean,default:false},
-    createdAt:{type:Date,default:Date.now},
-    role:{type:String ,enum:['admin','vendor','user'],default:'user'}
+const lostItemSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  dateLost: Date,
+  // Add other fields as needed
 });
-//create the model from the schema
-const LostItem=mongoose.model('LostItem',lostitemschema);
-//to use in user js we should export this mongoose 
-module.exports=LostItem; 
+
+module.exports = mongoose.model('LostItem', lostItemSchema);
